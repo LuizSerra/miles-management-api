29ae7c633475005e567e7576ca0f3109179cdeca
Luiz Serra <lserra.trade@gmail.com>
2023-06-12 09:49:11 -0300

    initial commit

---
+++ src/test/java/com/netmaxi/mm/api/MilesManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netmaxi.mm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilesManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
