faeefa78eb1c64834fbf5e38f07cb557496847c7
LuizSerra <lserra.trade@gmail.com>
2023-06-16 17:08:05 -0300

    Adicionando tratamento de erros

---
+++ src/main/java/com/netmaxi/mm/api/error/TratamentoDeErros.java
@@ -0,0 +1,30 @@
+package com.netmaxi.mm.api.error;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratamentoDeErros {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity tratarErro404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+		return ResponseEntity.badRequest().body(ex.getFieldErrors().stream().map(DadosErrosValidacao::new));
+	}
+	
+	private record DadosErrosValidacao(String campo, String Mensagem) {
+		public DadosErrosValidacao(FieldError field) {
+			this(field.getField(), field.getDefaultMessage());
+		}
+	}
+	
+}
